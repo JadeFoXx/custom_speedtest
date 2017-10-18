8e9f3fcf1c379ae9ee7165887730f53e914739c5
Jade <secure.ludwig.samuel@gmail.com>
2017-10-18 11:36:35 +0200

    minor changes; last todo still current;

---
+++ app/src/main/java/ludwigsamuel/custom_speedtest/util/ThreadController.java
@@ -0,0 +1,17 @@
+package ludwigsamuel.custom_speedtest.util;
+
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+/**
+ * Created by Ludwig Samuel on 10/18/2017.
+ */
+
+public class ThreadController extends TimerTask {
+
+
+    @Override
+    public void run() {
+
+    }
+}
